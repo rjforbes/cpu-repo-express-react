252ce9af0d2fbc0f248f0eea4a13eeacc6eefe1c
rj.forbes <rj.forbes@dplwireless.com>
2017-11-05 21:45:56 -0400

    - added Victory charts library and example in App.js

--- cpu-react/src/App.js
+++ cpu-react/src/App.js
@@ -1,21 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { VictoryChart, VictoryZoomContainer,VictoryLine,VictoryBrushContainer,VictoryAxis } from 'victory';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  handleZoom(domain) {
+    this.setState({selectedDomain: domain});
+  }
+
+  handleBrush(domain) {
+    this.setState({zoomDomain: domain});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{width:500}}>
+        <VictoryChart width={350} height={300} scale={{x: "time"}}
+          containerComponent={
+            <VictoryZoomContainer
+              zoomDimension="x"
+              zoomDomain={this.state.zoomDomain}
+              onZoomDomainChange={this.handleZoom.bind(this)}
+            />
+          }
+        >
+            <VictoryLine
+              style={{
+                data: {stroke: "tomato"}
+              }}
+              data={[
+                {a: new Date(1982, 1, 1), b: 125},
+                {a: new Date(1987, 1, 1), b: 257},
+                {a: new Date(1993, 1, 1), b: 345},
+                {a: new Date(1997, 1, 1), b: 515},
+                {a: new Date(2001, 1, 1), b: 132},
+                {a: new Date(2005, 1, 1), b: 305},
+                {a: new Date(2011, 1, 1), b: 270},
+                {a: new Date(2015, 1, 1), b: 470}
+              ]}
+              x="a"
+              y="b"
+            />
+
+          </VictoryChart>
+          <VictoryChart
+            padding={{top: 0, left: 50, right: 50, bottom: 30}}
+            width={550} height={100} scale={{x: "time"}}
+            containerComponent={
+              <VictoryBrushContainer
+                brushDimension="x"
+                brushDomain={this.state.selectedDomain}
+                onBrushDomainChange={this.handleBrush.bind(this)}
+              />
+            }
+          >
+            <VictoryAxis
+              tickFormat={(x) => new Date(x).getFullYear()}
+            />
+            <VictoryLine
+              style={{
+                data: {stroke: "tomato"}
+              }}
+              data={[
+                {key: new Date(1982, 1, 1), b: 125},
+                {key: new Date(1987, 1, 1), b: 257},
+                {key: new Date(1993, 1, 1), b: 345},
+                {key: new Date(1997, 1, 1), b: 515},
+                {key: new Date(2001, 1, 1), b: 132},
+                {key: new Date(2005, 1, 1), b: 305},
+                {key: new Date(2011, 1, 1), b: 270},
+                {key: new Date(2015, 1, 1), b: 470}
+              ]}
+              x="key"
+              y="b"
+            />
+          </VictoryChart>
       </div>
     );
   }
 }
+
+
 
 export default App;
